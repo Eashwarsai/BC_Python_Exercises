9ea5a18bd31f77a281b37357574fb972e73e39d9
Eashwarsai <Eashwarsaiboini@gmail.com>
2024-05-14 10:52:58 +0530

    remove inappropriate comments

--- functions_exercise.py
+++ functions_exercise.py
@@ -1,5 +1,4 @@
 def basic_calculator(total_value,*args,operator) :
-    #we can make use of switch case if incase we have multiple operators instead of only + and -
     if operator=='+' :
         return total_value+sum(args)
     elif operator=='-':
